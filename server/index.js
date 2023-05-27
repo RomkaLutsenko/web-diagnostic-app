c20eba1dd69644d8256d86f48d0e030209d37b73
roman <RomanLutsenko2001@gmail.com>
2023-05-28 02:33:50 +0700

    styles and others

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const authRouter = require("./routes/auth.routes");
+const mainRouter = require("./routes/main.routes");
 
 // Создаем сервер
 const app = express()
@@ -12,6 +13,7 @@ app.use(corsMiddleware)
 app.use(express.json())
 // Указываем ссылку, по которой роутер авторизации будет обрабатываться
 app.use("/api/auth", authRouter)
+app.use("/", mainRouter)
 
 // Функция которая подключается к БД и запускает сервер
 const start = async () => {
